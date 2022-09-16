a67523e99bbd1ded48c43b189f9c04cf13718774
Saloni <salonishah306@gmail.com>
2022-09-16 16:23:20 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from './components/layouts/Navbar';
+import Index from './components/layouts/Index';
+
+
+import {Provider} from './components/context';
 import './App.css';
-import Navbar from './components/Layouts/Navbar';
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
+class App extends Component {
+  render() {
+    return(
+      <Provider>
+      <Router>
+      <React.Fragment>
+    <Navbar/>
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Index}/>
+      </Switch>
     </div>
-  );
-}
+    </React.Fragment>
+    </Router>
+    </Provider>
+    );
+  };
+};
 
 export default App;
--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar=()=> {
   return (
     <nav className="navbar navbar-dark bg-dark mb-6">
-        <span className="navbar-brand mb-0 h1 mx-auto">Find Your Lyrics</span> 
+        <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
     </nav>
   );
 };
