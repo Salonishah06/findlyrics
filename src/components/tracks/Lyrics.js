2b6fb68add0250df89a8f4c306508bb606a2a614
Saloni <salonishah306@gmail.com>
2022-09-17 19:46:21 +0530

    Lyrics added

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Lyrics extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lyrics</h1>
+      </div>
+    )
+  }
+}
+export default Lyrics;
