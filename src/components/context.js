bd8f1568b46ef3e3f0264d43aa53b1cd2729686f
Saloni <salonishah306@gmail.com>
2022-09-17 19:32:34 +0530

    Tracks

--- src/components/context.js
+++ src/components/context.js
@@ -6,8 +6,18 @@ const Context = React.createContext();
 export class Provider extends Component {
   state = {
     track_list: [
-      { track: { track_name: "abc" } },
-      { track: { track_name: "123" } },
+      { track: { track_name: "Imagine Dragons" ,album_name: "Believer"} },
+      { track: { track_name: "One Direction",album_name: "What makes you beautiful" } },
+      { track: { track_name: "Justin Beiber",album_name: "Intentions" } },
+      { track: { track_name: "Ed Sheeran",album_name: "Perfect" } },
+      { track: { track_name: "The Vamps",album_name: "Somebody to You" } },
+      { track: { track_name: "Billie Elish",album_name: "Happier than ever" } },
+      { track: { track_name: "Taylor Swift",album_name: "August" } },
+      { track: { track_name: "Selena Gomez",album_name: "Calm down" } },
+      { track: { track_name: "Imagine Dragons",album_name: "Bones" } },
+   
+    
+      
     ],
     heading: "Top 10 Tracks",
   };
@@ -34,38 +44,3 @@ export class Provider extends Component {
 }
 
 export const Consumer = Context.Consumer;
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-
-// export const Context = React.createContext();
-
-// export function ContextController({ children }) {
-//   let intialState = {
-//     track_list: [],
-//     heading: ""
-//     // dispatch: action => this.setState(state => reducer(state, action))
-//   };
-
-//   const [state, setState] = useState(intialState);
-
-//   useEffect(() => {
-//     axios
-//       .get(
-//         `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
-//           process.env.REACT_APP_MM_KEY
-//         }`
-//       )
-//       .then(res => {
-//         // console.log(res.data);
-//         setState({
-//           track_list: res.data.message.body.track_list,
-//           heading: "Top 10 Tracks"
-//         });
-//       })
-//       .catch(err => console.log(err));
-//   }, []);
-
-//   return (
-//     <Context.Provider value={[state, setState]}>{children}</Context.Provider>
-//   );
-// }
